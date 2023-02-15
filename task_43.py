254965e49b1739ed01766a9968185c463b2aa9bb
VadimTalimonchik <tvadmins@gmail.com>
2023-02-15 17:32:47 +0300

    Задача 43 + варианты

--- task_43.py
+++ task_43.py
@@ -5,4 +5,30 @@
 # Ввод:           Вывод:
 # 1 2 3 2 3       2
 
-print('Hello 43')
+# -----------------------------
+
+# # вариант 1
+from random import randint
+
+list = [randint(0, 10) for i in range(int(input('N: ')))]
+print(list)
+k = 0
+
+for i in range(len(list) - 1):
+    for j in range(i + 1, len(list)):
+        if list[i] == list[j]:
+            k += 1
+print(k)
+
+# -----------------------------
+
+# # вариант 1
+# list = [10, 8, 10, 4, 8, 4, 8, 10, 4, 4, 4]
+# count = 0
+
+# for i in list:
+#     last_element = list.pop()
+#     if last_element in list:
+#         list.remove(last_element)
+#         count += 1
+# print(count)
